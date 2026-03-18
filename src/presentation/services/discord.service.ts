17c7cb51c578feeed5ac306ae6aca477ea05d4da
Ricardo-we <totigamer75@gmail.com>
2026-03-18 14:01:59 -0600

    Completed

--- src/presentation/services/discord.service.ts
+++ src/presentation/services/discord.service.ts
@@ -1,21 +1,19 @@
-import { APP_CONFIG } from "../../config/app-config"
+import { APP_CONFIG } from "../../config/app-config";
 
-export class DiscordService { 
-    constructor(private discordUrl: string = APP_CONFIG.DISCORD_URL ?? ""){}
+export class DiscordService {
+  constructor(private discordUrl: string = APP_CONFIG.DISCORD_URL ?? "") {}
 
-    async notify(message: string){
-        const body = {
-            content: message,
-        }
+  async notify(message: string) {
+    const body = {
+      content: message,
+    };
 
-        const res = await fetch(this.discordUrl,{
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json",
-            }
-        })
-
-        return res.json()
-    }
+    await fetch(this.discordUrl, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
