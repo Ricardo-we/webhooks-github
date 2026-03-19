d4fa08d7422d7398941a74d6d1af04fb741ec924
Ricardo-we <totigamer75@gmail.com>
2026-03-18 22:45:35 -0600

    Added signature

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import { APP_CONFIG } from "./config/app-config";
 import express from "express";
 import { GithubController } from "./presentation/controllers/controller";
+import { GithubSha256Middleware } from "./utils/verify-github-signature";
+
 
 function main() {
   const app = express();
-  const githubController = new GithubController();
-
+  
+  
   app.use(express.json());
+  app.use(GithubSha256Middleware.verifyGithubSignature)
+  
   app.listen(APP_CONFIG.PORT, () => {
     console.log(`Server is running on port ${APP_CONFIG.PORT}`);
   });
-
+  
+  
+  const githubController = new GithubController()
   app.post("/webhooks/github", githubController.githubWebhookHandler);
 }
 
